ac0a3811793bb7de923498cbb89172e16cd13aff
Cheng Yanru <yanru@cyanru.com>
2025-11-13 14:37:02 +0800

    Fix deployment issues for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
-import path from 'path'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   base: '/easy-reader/',
